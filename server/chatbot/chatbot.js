91e2084dbe341a754a2baa0142cf7bb4e316743a
YasmineGrati <yasmine.grati@esprit.tn>
2022-03-22 00:40:49 +0100

    Event query to dialogflow

--- server/chatbot/chatbot.js
+++ server/chatbot/chatbot.js
@@ -1,5 +1,6 @@
 'use strict'
 const dialogflow = require('dialogflow');
+const structjson = require('./structJson');
 const config = require('../config/keys');
 
 const sessionClient = new dialogflow.SessionsClient;
@@ -27,6 +28,29 @@ module.exports = {
         responses = await self.handleAction(responses);
         return responses;
     },
+
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+
+        //the event query request
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    //the query to send to the dialogflow agent
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters),
+                    //the language used by the client
+                    languageCode: config.dialogFlowSessionLanguageCode,
+                },
+            },
+        };
+        //send request and log result
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+    },
+
 
     handleAction: function(responses){
         return responses;
