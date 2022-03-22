9540482a290c416d80f445fa943095af1eaba74c
YasmineGrati <yasmine.grati@esprit.tn>
2022-03-22 16:07:17 +0100

    add two servers for devlopment
    server\package.json
    
    add script that runs both devlopment servers at once

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ require('./routes/dialogFlowRoutes')(app);
 
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`)
 })
