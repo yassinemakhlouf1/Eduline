999aaa7d3bb7c3cbf1edb6991c7b46544b77c744
YasmineGrati <yasmine.grati@esprit.tn>
2022-03-23 19:38:54 +0100

    add components + Routing

---
+++ server/client/src/components/pages/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Landing = () => (
+    <div style={{ textAlign: 'center' }}>
+        <h1>Hi, I'm Yasminos, Eduline's virtual assistant !! </h1>
+        I'm here to help you find your need.
+    </div>
+)
+
+export default Landing;
