2ae3020595ed0bb54dbeeeb279a82e8a7f93abf9
YasmineGrati <yasmine.grati@esprit.tn>
2022-03-26 22:04:52 +0100

    First frontend components

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => (
     <div style={{ textAlign: 'center' }}>
-        <h1>Hi, I'm Yasminos, Eduline's virtual assistant !! </h1>
+        <h1>Hi, I'm Minos, Eduline's virtual assistant !! </h1>
         I'm here to help you find your need.
     </div>
 )
