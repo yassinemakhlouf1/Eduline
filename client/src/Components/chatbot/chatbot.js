93ccb9624394c5edb4c88db9abab2f82608e878f
YasmineGrati <yasmine.grati@esprit.tn>
2022-04-11 11:51:17 +0100

    Update chatbot.js

--- client/src/Components/chatbot/chatbot.js
+++ client/src/Components/chatbot/chatbot.js
@@ -7,11 +7,17 @@ import Message from './Message';
 
 class Chatbot extends Component {
 
+    messagesEnd;
+    talkInput;
     constructor(props) {
         super(props);
+
+        // This binding is necessary to make `this` work in the callback
+        this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
+
         this.state = {
             messages: []
-        };
+        }
     }
 
     // text messages
@@ -54,6 +60,12 @@ class Chatbot extends Component {
         this.df_event_query('Welcome');
     }
 
+    componentDidUpdate() {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+        this.talkInput.focus();
+    }
+
+
     renderMessages(stateMessages) {
         if (stateMessages) {
             return stateMessages.map((message, i) => {
@@ -65,13 +77,23 @@ class Chatbot extends Component {
     }
 
 
+    _handleInputKeyPress(e) {
+        if (e.key === 'Enter') {
+            this.df_text_query(e.target.value);
+            e.target.value = '';
+        }
+    }
+
     render() {
-        return (           
-            <div style={{height: 400, width: 400, float: 'right'}}>
-                <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+        return (
+            <div style={{ height: 400, width: 400, float: 'right'}}>
+                <div id="chatbot" style={{ height: '100%', width: '100%', overflow: 'auto' }}>
                     <h2>Chatbot</h2>
                     {this.renderMessages(this.state.messages)}
-                    <input type="text"/>
+                    <div ref={(el) => { this.messagesEnd = el; }}
+                         style={{ float:"left", clear: "both" }}>
+                    </div>
+                    <input type="text" ref={(input) => { this.talkInput = input; }} onKeyPress={this._handleInputKeyPress}  />
                 </div>
             </div>
         )
