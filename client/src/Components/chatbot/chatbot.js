897a285e8159e7bf20e4a53d27336e60696aaa1d
YasmineGrati <yasmine.grati@esprit.tn>
2022-04-19 22:58:43 +0100

    Update chatbot.js
    Render card galleries on frontend.
    Now Chatbot can send simple text messages and cards messages

--- client/src/Components/chatbot/chatbot.js
+++ client/src/Components/chatbot/chatbot.js
@@ -113,14 +113,21 @@ class Chatbot extends Component {
 
     render() {
         return (
-            <div style={{ height: 400, width: 400, float: 'right'}}>
-                <div id="chatbot" style={{ height: '100%', width: '100%', overflow: 'auto' }}>
-                    <h2>Chatbot</h2>
+            <div style={{ height: 500, width: 400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
+                <nav>
+                    <div className="nav-wrapper">
+                        <a href="/" className="brand-logo">ChatBot</a>
+                    </div>
+                </nav>
+
+                <div id="chatbot" style={{ height: 388, width: '100%', overflow: 'auto' }}>
                     {this.renderMessages(this.state.messages)}
                     <div ref={(el) => { this.messagesEnd = el; }}
                          style={{ float:"left", clear: "both" }}>
                     </div>
-                    <input type="text" ref={(input) => { this.talkInput = input; }} onKeyPress={this._handleInputKeyPress}  />
+                </div>
+                <div className="col s12">
+                    <input style={{margin: 0, paddingLeft: '1%', paddingRight: '1%', width: '98%'}} placeholder="type your message:" type="text" ref={(input) => { this.talkInput = input; }} onKeyPress={this._handleInputKeyPress}  />
                 </div>
             </div>
         )
