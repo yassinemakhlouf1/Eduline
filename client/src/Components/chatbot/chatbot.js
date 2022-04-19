d0c4bc576c2c439a7866cdaf84461c3f4c256e7d
YasmineGrati <yasmine.grati@esprit.tn>
2022-04-19 22:29:59 +0100

    Create  a gallery of Cards
    Update chatbot.js

--- client/src/Components/chatbot/chatbot.js
+++ client/src/Components/chatbot/chatbot.js
@@ -4,6 +4,7 @@ import React, {Component} from 'react';
 import axios from "axios/index";
 
 import Message from './Message';
+import Card from './Card';
 
 class Chatbot extends Component {
 
@@ -67,14 +68,35 @@ class Chatbot extends Component {
     }
 
 
+    renderCards(cards) {
+        return cards.map((card, i) => <Card key={i} payload={card.structValue}/>);
+    }
+
+    renderOneMessage(message, i) {
+        if (message.msg && message.msg.text && message.msg.text.text) {
+            return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
+        } else if (message.msg && message.msg.payload.fields.cards) {
+            return <div key={i}>
+                <div className="card-panel grey lighten-5 z-depth-1">
+                    <div style={{overflow: 'hidden'}}>
+                       <div className="col s2">
+                           <a href="/" className="btn-floating btn-large waves-effect waves-light  teal lighten-2">{message.speaks}</a>
+                       </div>
+                       <div style={{ overflow: 'auto', overflowY: 'scroll'}}>
+                            <div style={{ height: 300, width:message.msg.payload.fields.cards.listValue.values.length * 270}}>
+                                {this.renderCards(message.msg.payload.fields.cards.listValue.values)}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
+        }
+    }
+
     renderMessages(stateMessages) {
         if (stateMessages) {
             return stateMessages.map((message, i) => {
-                if (message.msg && message.msg.text && message.msg.text.text) {
-                    return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
-                } else {
-                    return <h2>Cards</h2>;
-                }
+                return this.renderOneMessage(message, i);
                 });
         } else {
             return null;
