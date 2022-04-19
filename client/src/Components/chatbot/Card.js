5056b43bffe9d1582c5e3beaf9eff38cb6c2c297
YasmineGrati <yasmine.grati@esprit.tn>
2022-04-19 22:15:30 +0100

    Update Card.js

--- client/src/Components/chatbot/Card.js
+++ client/src/Components/chatbot/Card.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
 const Card = (props) => {
+  return (
     <div style={{ float: 'left', paddingRight: 30, width: 270 }}>
-         <div class="card">
-        <div class="card-image" style={{width: 240}}>
+         <div className="card">
+        <div className="card-image" style={{width: 240}}>
           <img alt={props.payload.fields.header.stringValue} src={props.payload.fields.image.stringValue} />
-          <span class="card-title">{props.payload.fields.header.stringValue}</span>
+          <span className="card-title">{props.payload.fields.header.stringValue}</span>
         </div>
-        <div class="card-content">
+        <div className="card-content">
            {props.payload.fields.description.stringValue}
            <p> <a href="/">{props.payload.fields.price.stringValue}</a></p>
         </div>
-        <div class="card-action">
+        <div className="card-action">
           <a target="_blank" rel="noreferrer" href={props.payload.fields.link.stringValue}>GET NOW</a>
         </div>
       </div>
     </div>
+  );
 }
 export default Card;
